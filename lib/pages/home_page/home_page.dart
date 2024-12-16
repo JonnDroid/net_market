2a68d000f788a5ff340c4bfabedf4d6cad449a12
JonnDroid <habbojonn123@gmail.com>
2024-12-16 14:31:38 +0800

    added price filter

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:net_market/core/theme.dart';
 import 'package:net_market/pages/widgets/category_list_view.dart';
 import 'package:net_market/pages/widgets/header.dart';
+import 'package:net_market/pages/widgets/price_filter.dart';
 import 'package:net_market/pages/widgets/search_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -20,6 +21,15 @@ class HomePage extends StatelessWidget {
               Header(text: 'Choose from any category'),
               CategoryListView(),
               Header(text: '10 products to choose from'),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Row(
+                  children: [
+                    PriceFilter(text: 'Lowest price first'),
+                    PriceFilter(text: 'Highest price first'),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
