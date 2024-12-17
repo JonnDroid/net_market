d68164873eb784a01ac76c823c2dd8469b13da78
JonnDroid <habbojonn123@gmail.com>
2024-12-17 15:30:03 +0800

    integrate fakestore api using retrofit + dio

---
+++ lib/data/service/api_service.dart
@@ -0,0 +1,19 @@
+import 'package:net_market/data/model/product_model.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import 'package:dio/dio.dart';
+
+part 'api_service.g.dart';
+
+const String BASE_URL = 'https://fakestoreapi.com/products?limit=50';
+
+@RestApi(baseUrl: BASE_URL)
+abstract class ApiService {
+  factory ApiService(Dio dio) = _ApiService;
+
+  @GET('products')
+  Future<List<ProductModel>> getProducts(@Query('limit') int limit);
+
+  @GET('')
+  Future<List<ProductModel>> getAllProducts();
+}
--- lib/presentation/pages/home_page/home_page.dart
+++ lib/presentation/pages/home_page/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:net_market/core/theme.dart';
+import 'package:net_market/data/model/product_model.dart';
+import 'package:net_market/data/service/api_service.dart';
 import '../widgets/category_list_view.dart';
 import '../widgets/header.dart';
 import '../widgets/price_filter.dart';
@@ -12,22 +15,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: DefaultColors.white,
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppSearchBar(),
-              Header(text: 'Choose from any category'),
-              CategoryListView(),
-              Header(text: '10 products to choose from'),
-              PriceFilter(),
-              ProductListView(),
-            ],
-          ),
+      body: _body(),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({
+    super.key,
+    required this.productList,
+  });
+  final List<ProductModel> productList;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: DefaultColors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const AppSearchBar(),
+            const Header(text: 'Choose from any category'),
+            const CategoryListView(),
+            Header(text: '${productList.length} products to choose from'),
+            const PriceFilter(),
+            const ProductListView(),
+          ],
         ),
       ),
     );
   }
+}
+
+FutureBuilder _body() {
+  final apiService =
+      ApiService(Dio(BaseOptions(contentType: "application.json")));
+  return FutureBuilder(
+      future: apiService.getAllProducts(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return HomeContent(
+            productList: snapshot.data != null ? snapshot.data! : const [],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      });
 }
